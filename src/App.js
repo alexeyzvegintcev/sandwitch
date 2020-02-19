a59071fd132452375061de3a100521562ce300cd
alexey.zvegintcev <alexey.zvegintcev@sysgears.com>
2020-02-19 11:58:27 +0200

    fixed typo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import './App.css';
+import Layout from './components/Layout/Layout'
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    
+      <div>
+      <Layout>
+        <p>test</p>
+      </Layout>
+      </div>
+   
   );
 }
 
